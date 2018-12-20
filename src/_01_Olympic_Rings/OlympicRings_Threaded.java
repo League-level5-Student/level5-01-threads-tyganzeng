fcec110de98533ff64c2af53d89e3a0c8a81a601
league <league@wts13.attlocal.net>
2018-12-19 18:28:27 -0800

    Working on robot race v2

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,52 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
+	public static void main(String[] args) {
+		Robot a = new Robot(400, 700);
+		Robot b = new Robot(800, 700);
+		Robot c = new Robot(1200, 700);
+		Robot d = new Robot(600, 300);
+		Robot e = new Robot(1000, 300);
+		
+		Thread r1 = new Thread(()->{
+			a.penDown();
+			a.setAngle(360);
+			a.move(360);
+		});
+		Thread r2 = new Thread(()->{
+			b.penDown();
+			b.setAngle(360);
+			b.move(360);
+		});
+		Thread r3 = new Thread(()->{
+			c.penDown();
+			c.setAngle(360);
+			c.move(360);
+		});
+		Thread r4 = new Thread(()->{
+			d.penDown();
+			d.setAngle(360);
+			d.move(360);
+		});
+		Thread r5 = new Thread(()->{
+			e.penDown();
+			e.setAngle(360);
+			e.move(360);
+		});
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		
+	
+		
+		
+	}
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,30 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Robot[] robot = new Robot[5];
+		//3. use a for loop to initialize the robots.
+		for(int i = 0; i<robot.length;i++) {
+			robot[i] = new Robot();
+			robot[i].setY(800);
+			robot[i].setX(100 + 300*i);
+		}
+		Random r = new Random();
+		
+		Thread r1 = new Thread(()->{
+			robot[0].move(r.nextInt(10));
+		});
+		
+		r1.start();
+		
+		//stop when robot reache stop
+	}
+	
+	
 }
