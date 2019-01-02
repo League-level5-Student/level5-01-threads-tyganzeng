09eff2634aac84305751b1cefb6292cbfdac8c17
tyganzeng <zengtygan@gmail.com>
2019-01-02 15:50:07 -0800

    working on advanced robot race

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -16,14 +16,54 @@ public class AdvancedRobotRace {
 			robot[i].setX(100 + 300*i);
 		}
 		Random r = new Random();
+		boolean reachedFinish = false;
 		
 		Thread r1 = new Thread(()->{
-			robot[0].move(r.nextInt(10));
+			robot[0].move(r.nextInt(170));
+
+		});
+		Thread r2 = new Thread(()->{
+			robot[1].move(r.nextInt(170));
+
+		});
+		Thread r3 = new Thread(()->{
+			robot[2].move(r.nextInt(170));
+
+		});
+		Thread r4 = new Thread(()->{
+			robot[3].move(r.nextInt(170));
+
+		});
+		Thread r5 = new Thread(()->{
+			robot[4].move(r.nextInt(170));
 		});
 		
 		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
 		
-		//stop when robot reache stop
+		/*while(!reachedFinish) {
+			if(robot[0].getY() < 0 || robot[1].getY() < 0 || robot[2].getY() < 0 || robot[3].getY() < 0 || robot[4].getY() < 0 ) {
+				reachedFinish = true;
+				if(robot[0].getY() < 0) {
+					System.out.println("Robot 0 is the winner!");
+				} else if(robot[1].getY() < 0) {
+					System.out.println("Robot 1 is the winner!");
+				} else if(robot[2].getY() < 0) {
+					System.out.println("Robot 2 is the winner!");
+				} else if(robot[3].getY() < 0) {
+					System.out.println("Robot 3 is the winner!");
+				} else if(robot[4].getY() < 0) {
+					System.out.println("Robot 4 is the winner!");
+				} 
+				
+			}
+		}*/
+		
+		
+
 	}
 	
 	
